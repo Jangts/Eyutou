5c84b351392ffbb8d514f772b99f4d87fe0a5d1a
Jangts <microivan@live.com>
2018-06-01 19:08:07 +0800

    1908

---
+++ frontend/script/main.js
@@ -0,0 +1,20 @@
+var title = new Vue({
+    el: '#title',
+    data: {
+        title: '鳄鱼投'
+    }
+});
+
+var main = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var nav = new Vue({
+    el: '#tabs',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
