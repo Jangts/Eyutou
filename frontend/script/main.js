ec8168b29e8d1bfe53400868d789ac2d464bed19
Jangts <microivan@live.com>
2018-06-07 20:35:09 +0800

    2034

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -1,83 +1,129 @@
+/*!
+ * tanguage script compiled code
+ *
+ * Datetime: Thu, 07 Jun 2018 10:54:52 GMT
+ */
 ;
-(function() {
-    let myStorage = {
-        province: provinces["42"],
-        list: null
-    };
-    const title = new Vue({
-        el: '#title',
-        data: {
-            title: '鳄鱼投'
-        }
-    });
-
-    const homeComponent = {
-        template: '#homeTemplate',
-        data() {
-            return {
-                province: myStorage.province,
-                list: myStorage.list
-            }
-        }
-    }
-    const projectComponent = { template: '#projectTemplate' }
-    const circleComponent = { template: '#circleTemplate' }
-    const mineComponent = { template: '#mineTemplate' }
-
-    const main = new Vue({
-        el: '#main',
-        data: {
-            province: provinces[42],
-            tabs: [{
-                    path: "/home",
-                    classname: "home",
-                    name: "首页"
-                },
-                {
-                    path: "/project",
-                    classname: "project",
-                    name: "工程"
-                },
-                {
-                    path: "/circle",
-                    classname: "circle",
-                    name: "圈子"
-                },
-                {
-                    path: "/mine",
-                    classname: "mine",
-                    name: "我的"
-                }
-            ]
-        },
-        router: new VueRouter({
-            linkActiveClass: 'active',
-            linkExactActiveClass: 'active',
-            routes: [
-                { path: '/', redirect: '/home' },
-                { path: '/home/', component: homeComponent },
-                { path: '/home/:province', component: homeComponent },
-                { path: '/project', component: projectComponent },
-                { path: '/circle', component: circleComponent },
-                { path: '/mine', component: mineComponent }
-            ]
-        }),
-        watch: {
-            // 如果路由有变化，会再次执行该方法
-            '$route': 'fetchData'
-        },
-        created() {
-            // 组件创建完后获取数据，
-            // 此时 data 已经被 observed 了
-            this.storeData()
-        },
-        methods: {
-            fetchData() {
-                myStorage.province = (this.$route.params.province && provinces[this.$route.params.province]) || myStorage.province;
-            },
-            storeData() {
-                console.log(this.$route.params);
-            }
-        }
-    });
-}());
+// tang.config({});
+tang.init().block([
+	'$_/async/'
+], function (pandora, root, imports, undefined) {
+	var _ = pandora;
+	var myStorage = {
+		provinceCode: "42",
+		list: null
+	};
+	var title = new Vue({
+		el: '#title',
+		data: {
+			title: '鳄鱼投'
+		}
+	});
+	var homeComponent = {
+		template: '#homeTemplate',
+		data: function () {
+			var provinceCode = void 0;var provinceInfo = void 0;
+			if (myStorage.provinceCode && provinces[myStorage.provinceCode]) {
+				provinceCode = myStorage.provinceCode;
+				provinceInfo = provinces[provinceCode];
+			}
+			else {
+				provinceCode = "42";
+				provinceInfo = provinces["42"];
+			}
+			var self = this;
+			var url = '/json/home/hubei/projects.json';
+			_.async.json(url, function (result) {
+				root.console.log(self, result);
+				self.list = result.data;
+			});
+			return {
+				province: provinceInfo,
+				tags: null,
+				list: myStorage.list
+			};
+		},
+		created: function () {
+			var self = this;
+			setTimeout(function () {
+				self.tags = [{name: "abc"}, {name: "abc"}, {name: "abc"}, {name: "abc"}, {name: "abc"}, {name: "abc"}, {name: "abc"}, {name: "abc"}, {name: "abc"}];
+			}, 2000);
+		}
+	};
+	var projectComponent = {template: '#projectTemplate'};
+	var circleComponent = {template: '#circleTemplate'};
+	var mineComponent = {template: '#mineTemplate'};
+	var main = new Vue({
+		el: '#main',
+		data: {
+			province: provinces[42],
+			tabs: [{
+				path: "/home",
+				classname: "home",
+				name: "首页"
+			}, {
+				path: "/project",
+				classname: "project",
+				name: "工程"
+			}, {
+				path: "/circle",
+				classname: "circle",
+				name: "圈子"
+			}, {
+				path: "/mine",
+				classname: "mine",
+				name: "我的"
+			}]
+		},
+		router: new VueRouter({
+			linkActiveClass: 'active',
+			linkExactActiveClass: 'active',
+			routes: [{
+				path: '/',
+				redirect: '/home'
+			}, {
+				path: '/home/',
+				component: homeComponent
+			}, {
+				path: '/home/:province',
+				component: homeComponent
+			}, {
+				path: '/project',
+				component: projectComponent
+			}, {
+				path: '/circle',
+				component: circleComponent
+			}, {
+				path: '/mine',
+				component: mineComponent
+			}]
+		}),
+		watch: {
+			'$route': 'fetchData'
+		},
+		created: function () {
+			this.storeData();
+		},
+		methods: {
+			fetchData: function () {
+				myStorage.provinceCode = this.$route.params.province || "42";
+			},
+			storeData: function () {
+				root.console.log(this.$route.params);
+			}
+		}
+	});
+	var searchComponent = {template: '#searchComponent'};
+	var popup = new Vue({
+		el: '#popup',
+		router: new VueRouter({
+			linkActiveClass: 'active',
+			linkExactActiveClass: 'active',
+			routes: [{
+				path: '/search/',
+				component: searchComponent
+			}]
+		})
+	});
+}, true);
+//# sourceMappingURL=main.js.map
